c379924c41b3fda5853a77f49594dc3b401c2736
Larry Barrios Jr <github@larrybarriosjr.com>
2021-05-03 13:17:43 +0800

    Add react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./app/App"
+import { QueryClient, QueryClientProvider } from "react-query"
+import App from "app/App"
 import reportWebVitals from "./reportWebVitals"
+import "./index.css"
+
+const queryClient = new QueryClient()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 )
