3880c6334db9cc4a77da7228c2aa4c8d5f07dcac
Larry Barrios Jr <github@larrybarriosjr.com>
2021-05-03 13:16:18 +0800

    Add environment variables

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: "development" | "production" | "test"
+    PUBLIC_URL: string
+    REACT_APP_SKYSCANNER_API_URL: string
+    REACT_APP_RAPIDAPI_KEY: string
+    REACT_APP_RAPIDAPI_HOST: string
+    REACT_APP_SKYSCANNER_REFERENCE_URL: string
+    REACT_APP_SKYSCANNER_QUERY_URL: string
+    REACT_APP_SKYSCANNER_ROUTES_URL: string
+    REACT_APP_SKYSCANNER_QUOTES_URL: string
+    REACT_APP_SKYSCANNER_DATES_URL: string
+  }
+}
