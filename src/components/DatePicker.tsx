d4a391385c7bdf03d21575eaa12112932aa6cdcd
Larry Barrios Jr <github@larrybarriosjr.com>
2021-05-06 14:58:29 +0800

    Add Regular Date Picker component

---
+++ src/components/DatePicker.tsx
@@ -0,0 +1,65 @@
+import DayPickerInput from "react-day-picker/DayPickerInput"
+import { DateUtils, DayPickerInputProps } from "react-day-picker"
+import dateFnsFormat from "date-fns/format"
+import dateFnsParse from "date-fns/parse"
+
+type DatePickerProps = DayPickerInputProps & {
+  date: string
+  setDate: React.Dispatch<React.SetStateAction<string>>
+}
+
+const DatePicker = ({ date, setDate, ...props }: DatePickerProps) => {
+  const DATE_FORMAT_INPUT = "dd MMMM y"
+  const DATE_FORMAT_STATE = "yyyy-MM-dd"
+
+  const handleDayChange = (day: Date) => {
+    if (day instanceof Date) {
+      const formattedDate = dateFnsFormat(day, DATE_FORMAT_STATE)
+      setDate(formattedDate)
+    }
+  }
+
+  const formatDate = (date: Date, format: string) => {
+    return dateFnsFormat(date, format)
+  }
+
+  const parseDate = (str: string, format: string) => {
+    const parsedDate = dateFnsParse(str, format, new Date())
+    if (DateUtils.isDate(parsedDate)) {
+      return parsedDate
+    }
+  }
+
+  const modifiers = {
+    days: { before: new Date(), after: new Date() }
+  }
+
+  const modifiersStyles = {
+    outside: { cursor: "pointer" },
+    today: { width: "2.5rem", lineHeight: "1.25rem" },
+    days: { width: "2.5rem", lineHeight: "1.25rem" }
+  }
+
+  return (
+    <DayPickerInput
+      onDayChange={handleDayChange}
+      format={DATE_FORMAT_INPUT}
+      formatDate={formatDate}
+      parseDate={parseDate}
+      inputProps={{
+        readOnly: true,
+        className: date ? "font-bold" : ""
+      }}
+      dayPickerProps={{
+        modifiers,
+        modifiersStyles,
+        fromMonth: new Date(),
+        showOutsideDays: true,
+        enableOutsideDaysClick: true
+      }}
+      {...props}
+    />
+  )
+}
+
+export default DatePicker
