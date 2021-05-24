d181eb4a5c288969600b2bf0e76fb0e9f6b09c63
Larry Barrios Jr <github@larrybarriosjr.com>
2021-05-24 12:43:05 +0800

    Combine flight date and details in one component

--- src/components/form/booking/FlightDetails.tsx
+++ src/components/form/booking/FlightDetails.tsx
@@ -2,7 +2,12 @@ import FlightCarrier from "components/results/FlightCarrier"
 import FlightPrice from "components/results/FlightPrice"
 import FlightRoute from "components/results/FlightRoute"
 import Col from "containers/Col"
+import Row from "containers/Row"
+import Section from "containers/Section"
+import { format } from "date-fns"
+import { DateFormat } from "defaults/flight"
 import { FlightData } from "types/app"
+import { isRoundTrip } from "utils/boolean"
 
 type FlightDetailsProps = {
   data: FlightData
@@ -11,24 +16,40 @@ type FlightDetailsProps = {
 const FlightDetails = ({ data }: FlightDetailsProps) => {
   const { carrier, currency, destination, item, origin } = data
 
+  const departureDate = new Date(data.item.OutboundLeg.DepartureDate)
+  const returnDate = isRoundTrip(data.item)
+    ? new Date(data.item.InboundLeg.DepartureDate)
+    : undefined
+
   return (
-    <div
-      className={`flex justify-between items-center p-4 m-6 rounded-3xl
+    <Section>
+      <Row>
+        <p className="text-blue-900 font-bold">
+          Flight Details{" "}
+          <span className="font-normal">
+            ({format(departureDate, DateFormat.DISPLAY)}
+            {returnDate ? " to " + format(returnDate, DateFormat.DISPLAY) : ""})
+          </span>
+        </p>
+      </Row>
+      <div
+        className={`flex justify-between items-center p-4 m-6 rounded-3xl
             shadow border border-green-200 bg-green-50 text-blue-900`}
-    >
-      <Col w="4/12">
-        <FlightRoute route={origin} />
-      </Col>
-      <Col w="2/12">
-        <FlightCarrier item={item} carrier={carrier} />
-      </Col>
-      <Col w="4/12">
-        <FlightRoute route={destination} />
-      </Col>
-      <Col w="2/12">
-        <FlightPrice item={item} currency={currency} booking />
-      </Col>
-    </div>
+      >
+        <Col w="4/12">
+          <FlightRoute route={origin} />
+        </Col>
+        <Col w="2/12">
+          <FlightCarrier item={item} carrier={carrier} />
+        </Col>
+        <Col w="4/12">
+          <FlightRoute route={destination} />
+        </Col>
+        <Col w="2/12">
+          <FlightPrice item={item} currency={currency} booking />
+        </Col>
+      </div>
+    </Section>
   )
 }
 
--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -1,14 +1,10 @@
 import BookingForm from "components/form/booking/BookingForm"
-import FlightDate from "components/form/booking/FlightDate"
 import FlightDetails from "components/form/booking/FlightDetails"
-import Row from "containers/Row"
-import Section from "containers/Section"
 import { LocalStorageKey } from "defaults/web"
 import { times } from "lodash"
 import { Fragment, useEffect } from "react"
 import { useLocation } from "react-router"
 import { ReactRouterState } from "types/app"
-import { isRoundTrip } from "utils/boolean"
 
 const BookingPage = () => {
   const location = useLocation<ReactRouterState>()
@@ -20,19 +16,7 @@ const BookingPage = () => {
 
   return (
     <Fragment>
-      <Section>
-        <Row>
-          <FlightDate
-            departureDate={new Date(data.item.OutboundLeg.DepartureDate)}
-            returnDate={
-              isRoundTrip(data.item)
-                ? new Date(data.item.InboundLeg.DepartureDate)
-                : undefined
-            }
-          />
-        </Row>
-        <FlightDetails data={data} />
-      </Section>
+      <FlightDetails data={data} />
       {times(data.passengers, idx => (
         <BookingForm key={idx} id={idx + 1} />
       ))}
