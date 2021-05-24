265a6a46992f913cc878aaed3fea01ee8a4946b3
Larry Barrios Jr <github@larrybarriosjr.com>
2021-05-24 12:33:22 +0800

    Make logo component route to home page

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,13 @@
 import logo from "assets/bifrost.png"
+import { Link } from "react-router-dom"
 
 type LogoProps = React.ComponentPropsWithoutRef<"img">
 
 const Logo = ({ ...props }: LogoProps) => {
   return (
-    <div className="bg-white shadow rounded-full py-4 px-8">
+    <Link to="/" className="bg-white shadow rounded-full py-4 px-8">
       <img src={logo} alt="Bifrost Flight Booking System" {...props} />
-    </div>
+    </Link>
   )
 }
 
