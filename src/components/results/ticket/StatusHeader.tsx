85ba18e178b3f0d370485522193f1a07f7176944
Larry Barrios Jr <github@larrybarriosjr.com>
2021-05-27 18:23:26 +0800

    Add barcode on print

--- src/components/results/ticket/StatusHeader.tsx
+++ src/components/results/ticket/StatusHeader.tsx
@@ -1,12 +1,18 @@
 import Col from "containers/Col"
 import Row from "containers/Row"
 import Section from "containers/Section"
+import { useBarcode } from "react-barcodes"
 
 type StatusHeaderProps = {
   reference: string
 }
 
 const StatusHeader = ({ reference }: StatusHeaderProps) => {
+  const { inputRef } = useBarcode({
+    value: reference,
+    options: { text: "Bifrost_Reference_Code_".toUpperCase() + reference }
+  })
+
   const handlePrint = () => {
     window.print()
   }
@@ -23,10 +29,15 @@ const StatusHeader = ({ reference }: StatusHeaderProps) => {
           <p className="text-sm">
             Please check your email for the details of your booking.
           </p>
+          <img
+            ref={inputRef}
+            alt="Booking barcode"
+            className="w-80 mt-2 self-center hidden print:block"
+          />
           <button
             type="button"
             onClick={handlePrint}
-            className="flex justify-center items-center h-10 w-40 mx-auto mb-2 mt-4
+            className="flex justify-center items-center h-10 w-40 mx-auto my-2
               font-bold bg-blue-900 text-gray-50 border-2 border-blue-900 rounded-full
               focus:outline-none focus:border-blue-500 hover:border-gray-400 print:hidden"
           >
